685e9c0f1c7a8c0eee554cb3387c211089f4d03b
Njuelle <n.juelle@gmail.com>
2016-09-28 10:58:49 +0200

    fix for pull request #53

--- app/src/main/java/com/hbdriod/kata/StringCalculator.java
+++ app/src/main/java/com/hbdriod/kata/StringCalculator.java
@@ -11,9 +11,7 @@ public class StringCalculator {
     public int add (String string) {
         String delimiter = "\n,";
         if (!string.isEmpty()) {
-            if (string.equals("1")) {
-                return 1;
-            } else if (string.startsWith("//")){
+            if (string.startsWith("//")){
                 delimiter = String.valueOf(string.charAt(2));
                 string = string.substring(4);
             }
